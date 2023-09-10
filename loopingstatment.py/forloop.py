a2e2ce42ac25e14342a408424db78d8185bf5092
saikumarmudumunthala <mudumunthalasaikumar04@gmail.com>
2023-09-10 15:59:18 +0530

    ok

--- loopingstatment.py/forloop.py
+++ loopingstatment.py/forloop.py
@@ -11,3 +11,31 @@ for i in l1:
     for j in l2:
         print(i,j)
         
+
+
+for i in range(10):
+    print(i)   
+
+l3 = ["saikumar","mounika","bolla"]
+for i in l3:
+    print(i)         
+
+
+E1 = ["number","id","email","role"]
+for i in E1:
+    print(i)    
+
+S1 = ["roll1","branch","college","id"]
+S2 = ["exam","lab","records"]
+for i in S1:
+    for j in S2:
+        print(i,j)
+
+
+
+fruits1 = ["apple","mango","banana","grape","watermilon"]
+fruits2 = [22,33,74]
+for i in fruits1:
+    for j in fruits2:
+        print(i,j)
+ 
